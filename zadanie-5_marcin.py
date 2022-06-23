0e85dd8e54bcb526932e6c763365dd8cab659c51
Gołub Marcin <Marcin.Golub@canalplus.pl>
2022-06-23 15:25:13 +0200

    dodany pusty plik

---
+++ zadanie-5_marcin.py
@@ -0,0 +1,16 @@
+# Atrybuty:
+# # # # name – ‘superman’
+# # # # superpowers – lista supermocy np [‘ab’, ‘bc’, ‘cd’]
+# # # # life_points – randomowa liczba z zakresu 1-10 - biblioteka random (random.randint)
+# # # #
+# https://docs.python.org/3/library/random.html#random.randint
+# random.choice : https://docs.python.org/3/library/random.html#random.choice
+# # #
+# # # # Metody:
+# # # # attack – return randomowa liczba z zakresu 1-10
+# # # # decrease_life  - obniża liczbe punktów życia o podaną ilość
+# # # # (x wchodzi jako parameter do funkcji: def decrease_life(self, x) )
+# # #
+# # # # Tworze obiekt poprzez: superman = Superman()
+# import superman from zadanie_superheroes
+#
