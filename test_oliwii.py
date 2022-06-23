cf347aae744fad53685c3e4408d0f9807c219525
oliwia.wojtkowska <oliwia.wojtkowska@itmagination.com>
2022-06-23 15:02:57 +0200

    added new function to print customer name

--- test_oliwii.py
+++ test_oliwii.py
@@ -1,8 +1,11 @@
 class BankAccount:
-    def __init__(self, saldo):
+    def __init__(self, saldo, name):
         self.saldo = saldo
+        self.name = name
 
     def show_saldo(self):
         print(f'masz {self.saldo} zł')
 
+    def print_name(self):
+        print(f" nazywam sie {self.name}")
 
