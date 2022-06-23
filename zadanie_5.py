07611fd699e0900b681946f3d1819607a0054f1e
Kmajcher <kasiamajcher60@gmail.com>
2022-06-23 15:59:31 +0200

    names changes

--- zadanie_5.py
+++ zadanie_5.py
@@ -7,14 +7,14 @@ class Hero:
         self.life_points = random.randint(1, 10)
 
     def attack(self):
-        self.life_points = random.randint(1, 10)
-        return self.life_points
+        random_number = random.randint(1, 10)
+        return random_number
 
     def decrease_life(self, decrease_points):
         self.life_points -= decrease_points
         return self.life_points
 
-my_hero = Hero("Sherlock", 'deduction')
+my_hero = Hero("Sherlock Holmes", "deduction")
 print(my_hero.name, my_hero.superpowers, my_hero.life_points)
 print(my_hero.attack())
 print(my_hero.decrease_life(3))
