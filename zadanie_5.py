b6dd5da30ded0b0d0c0403a884e9c38575b20722
Kmajcher <kasiamajcher60@gmail.com>
2022-06-23 15:20:49 +0200

    first commit

---
+++ zadanie_5.py
@@ -0,0 +1,13 @@
+import random
+
+class Heroes:
+    def __init__(self, name, superpowers, life_points):
+        self.name = name
+        self.superpowers = superpowers
+        self.life_points = life_points
+
+
+
+
+
+
