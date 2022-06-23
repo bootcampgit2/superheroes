729486234fe51177d7d5b5971a8062133e99617e
Kmajcher <kasiamajcher60@gmail.com>
2022-06-23 15:44:27 +0200

    add hero

--- zadanie_5.py
+++ zadanie_5.py
@@ -1,11 +1,23 @@
 import random
 
-class Heroes:
+class Heroe:
     def __init__(self, name, superpowers, life_points):
         self.name = name
         self.superpowers = superpowers
         self.life_points = life_points
 
+    def attack(self):
+        self.life_points = random.randint(1, 10)
+        return self.life_points
+
+    def decrease_life(self, decrease_points):
+        self.life_points -= decrease_points
+        return self.life_points
+
+my_hero = Heroe("Sherlock", 'deduction', 100)
+print(my_hero.name, my_hero.superpowers, my_hero.life_points)
+print(my_hero.attack())
+print(my_hero.decrease_life(3))
 
 
 
